943ff16486eaffc162e97209fafee6d5d6806077
Sufiyan Ahmed <100953582+Sufiyan92@users.noreply.github.com>
2023-02-05 21:56:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict'
+let cel = document.querySelector('.c')
+let feh = document.querySelector('.f')
+
+cel.addEventListener('input', function () {
+    let c = this.value
+    let f = (c * 9 / 5) + 32
+    if (!Number.isInteger(f)) {
+        f = f.toFixed(4)
+    }
+    feh.value = f
+})
+
+feh.addEventListener('input', function () {
+    let f = this.value
+    let c = (f - 32) * 5 / 9
+    if (!Number.isInteger(c)) {
+        c = c.toFixed(4)
+    }
+    cel.value = c
+})
+
+// cel.style.background = 'black'
